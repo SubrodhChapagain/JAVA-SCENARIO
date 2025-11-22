7e8d586b69daa09de8d460d42163ce772bf4e3f2
Subrodh Chapagain <chapagainsubrodh@gmail.com>
2025-11-22 13:22:31 +0545

    Initial sharing of project

---
+++ rickshaw.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class rickshaw
+{
+    public static void main (String[]args){
+    
+    Scanner input = new Scanner (System.in);
+    System.out.println("ENTER DISTANCE in Km");
+    double Dis = input.nextDouble();
+    System.out.println("distance in kilometer"+ Dis+"Km");
+    input.nextLine();
+     
+    System.out.println("ENTER TIME in minute");
+    int time = input.nextInt();
+    System.out.println("Time in minutes is"+ time+"min");
+    input.nextLine();
+    
+    System.out.println("IS CUSTOMER LOCAL?");
+    String islocal = input.next();
+    
+    String check = (islocal.equalsIgnoreCase("yes"))?"CUSTOMER IS LOCAL":"CUSTOMER IS NOT LOCAL";
+    System.out.println(check);
+    
+    System.out.println("IS RIDE NIGHT ?");
+    String ride = input.next();
+    
+    String checkr = (ride.equalsIgnoreCase("yes"))?"NIGHT RIDE":"DAY RIDE";
+    System.out.println(checkr);
+    
+    
+    int basefare = 25;
+    int perkilometercharge = 10;
+    int permincharge = 5;
+    
+    double total = Dis*perkilometercharge +time*permincharge + basefare;
+    System.out.println("Actual fare "+total);
+    
+    double total1 = check.equalsIgnoreCase("CUSTOMER IS LOCAl") ? total * 0.9 : total; //10%
+    System.out.println("Total charge after discount(only for locals); "+total1);
+     
+    double total2= checkr.equalsIgnoreCase("night ride") ? total1*1.15 : total; //15%
+    System.out.println("Total charge for night ride "+total2);
+    
+   
+    
+    }
+}
